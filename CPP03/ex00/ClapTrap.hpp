839b2c58310eb78bca4b7b1c70b352fa14e50664
barodrig <barodrig@student.42.fr>
2022-07-26 15:17:10 +0200

    CPP03/ex00 done

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 18:59:09 by barodrig          #+#    #+#             */
-/*   Updated: 2022/07/25 19:08:41 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/07/26 14:59:39 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,24 @@
 
 # include <iostream>
 
-# define HP=1
-# define EP=2
-# define DMG=3
-
 class   ClapTrap{
-    private :
+    
+    private:
         std::string _name;
         int         _hp;
         int         _ep;
         int         _dmg;
 
-    public  :
+    public :
         ClapTrap( void );
-        ClapTrap( std::string nickname );
+        ClapTrap( std::string const nickname );
         ClapTrap( ClapTrap const & src );
         ~ClapTrap( void );
-        ClapTrap & operator=(ClapTrap const & rhs);
-        
-        int     getAttribute(int index);
-        void    attack(const std::string & target);
-        void    takeDamage(unsigned int amount);
-        void    beRepaired(unsigned int amount);
+        ClapTrap & operator=( ClapTrap const & rhs );
+
+        void    attack( const std::string & target );
+        void    takeDamage( unsigned int amount );
+        void    beRepaired( unsigned int amount );
 };
 
 #endif
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 18:58:19 by barodrig          #+#    #+#             */
-/*   Updated: 2022/07/25 18:58:42 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/07/26 15:12:14 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,33 @@
 
 int main(void)
 {
+
+    std::cout << "Test 1 = One defaultly constructed ClapTrap + One namely constructed" << std::endl;
+    ClapTrap Mike;
+    ClapTrap Johhny("JohnnyNoxville");
+
+    Mike.attack("JohnnyNoxville");
+    Johhny.takeDamage(0);
+    Johhny.beRepaired(10);
+    Johhny.attack("Mike");
+    Mike.takeDamage(0);
+    Mike.beRepaired(10);
+    std::cout << "- - - - - - - - - - - - - - - - - - - - -" << std::endl << std::endl;
+
+    std::cout << "Test 2 = One defaultly constructed ClapTrap + One copy constructed" << std::endl;
+    ClapTrap Mike2;
+    ClapTrap Mike2bis(Mike2);
+    int i = 0;
+    while (i++ < 11)
+        Mike2bis.beRepaired(10);
+    std::cout << "- - - - - - - - - - - - - - - - - - - - -" << std::endl << std::endl;
+
+    std::cout << "Test 3 = One defaultly constructed ClapTrap + One copy constructed" << std::endl;
+    ClapTrap Mike2bisbis(Mike2bis);
+    i = 0;
+    while (i++ < 10)
+        Mike2bisbis.attack("JohnnyNoxville");
+    std::cout << "- - - - - - - - - - - - - - - - - - - - -" << std::endl << std::endl;
+
     return (0);
 }
