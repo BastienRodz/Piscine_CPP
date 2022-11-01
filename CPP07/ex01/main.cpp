39c8287761d584b8336cf148ff19e8976bedfabb
Bastien RODRIGUEZ <barodrig@e2r2p8.clusters.42paris.fr>
2022-11-01 16:21:21 +0100

    Corrected my Cpp07/main.cpp

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 11:01:59 by barodrig          #+#    #+#             */
-/*   Updated: 2022/11/01 11:02:11 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/11/01 16:20:02 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 int main()
 {
+	float float_tab[5] = {10.0f, 1.0f, 20.0f, 42.42f, 24.24f};
+	int int_tab[5] = {10, 8, 6, 4, 2};
+	const char char_tab[5] = "test";
 
-        int int_array[5] = {1, 2, 3, 4, 5};
-        const char ch_array[6] = "hello";
-        float fl_array[3] = {2.5f, 3.4f, 4.0f};
-
-        std::cout << "\033[1m\033[36m INT ARRAY " << std::endl;
-        ::iter(int_array, sizeof(int_array) / sizeof(int), &print);
-        std::cout << "\033[1m\033[36m CHAR ARRAY " << std::endl;
-        ::iter(ch_array, strlen(ch_array), &print);
-        std::cout << "\033[1m\033[36m FLOAT ARRAY " << std::endl;
-        ::iter(fl_array, sizeof(fl_array) / sizeof(int), &print);
-    
+	std::cout << " FLOAT tab " << std::endl;
+	::iter(float_tab, sizeof(float_tab) / sizeof(int), &print);
+	std::cout << " INT tab " << std::endl;
+	::iter(int_tab, sizeof(int_tab) / sizeof(int), &print);
+	std::cout << " CHAR tab " << std::endl;
+	::iter(char_tab, strlen(char_tab), &print);
 }
