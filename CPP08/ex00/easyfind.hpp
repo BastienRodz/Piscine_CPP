af9aec5f16e6d89b7e1cdf0d5fb373f5d926c31d
Bastien RODRIGUEZ <barodrig@e2r2p8.clusters.42paris.fr>
2022-11-02 15:48:25 +0100

    Cpp08/ex00 main ok !

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 20:23:07 by barodrig          #+#    #+#             */
-/*   Updated: 2022/11/02 09:34:32 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/11/02 15:33:32 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,29 @@
 # include <exception>
 
 template<typename T>
-void	easyfind( T &cont, int i )
+typename T::const_iterator	easyfind( T &cont, int const i )
 {
 	typename T::iterator it = find(cont.begin(), cont.end(), i); 
-	if ( it == cont.end() )
-		std::cout << "No occurence of " << i << " were found in our container." << std::endl;
-	else
-		std::cout << "An occurence of " << i << "was found at " << *it << " in our container." << std::endl;
+	try 
+	{
+		if( it == cont.end() )
+			std::cerr << "No occurence of " << i << " were found in our container." << std::endl;
+		else
+			std::cout << "An occurence of " << i << " was found at position " << std::distance(cont.begin(), it) + 1 << " in our container." << std::endl;
+	}
+	catch (const std::exception &error)
+	{
+		std::cerr << error.what() << "for i = " << i << std::endl;
+	}
+	return (static_cast<typename T::const_iterator>(it));
+};
+
+class NoOccurence : public std::exception
+{
+	virtual const char *what() const throw()
+	{
+		return ("No occurence found ");
+	}
 };
 
 template<typename T>
@@ -33,9 +49,10 @@ void	printer( T &cont )
 {
 	typename T::iterator it = cont.begin(); 
 	typename T::iterator end = cont.end();
-	while ( it++ != end )
+	while ( it != end )
 	{
 		std::cout << *it << ", ";
+		it++;
 	}
 };
 
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,133 +6,136 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 20:23:15 by barodrig          #+#    #+#             */
-/*   Updated: 2022/11/02 09:34:38 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/11/02 15:41:22 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <vector>
+#include <stack>
 #include <deque>
 #include <list>
 
-void test1()
+void    test1()
 {
-    std::cout <<  "\033[1m\033[35m VECTOR TESTS" << std::endl << std::endl;
-   std::cout <<  "\033[0m\033[34m FOUND\033[0m\033[37m" << std::endl;
-    const int N = 5;
-    int a[N] = {1, 4, 7, 3};
-    std::vector<int> vec(a, a + N);
-    printer(vec);
-    easyfind(vec, 7);
-    std::cout << std::endl;
-
-}
-
-void test2()
-{
-   std::cout <<  "\033[0m\033[34m NOT FOUND\033[0m\033[37m" << std::endl;
-    const int N = 5;
-    int a[N] = {1, 4, 7, 3};
-    std::vector<int> vec(a, a + N);
-    printer(vec);
-    easyfind(vec, 8);
-
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_1 = VECTOR + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;32mFOUND\033[0m" << std::endl;
+    const int n = 10;
+    int tab[n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> vector(tab, tab + n);
+    printer(vector);
+    easyfind(vector, 9);
     std::cout << std::endl;
 }
 
-void test3()
+void    test2()
 {
-   std::cout <<  "\033[0m\033[34m EMPTY VECTOR \033[0m\033[37m" << std::endl;
-    std::vector<int> vec;
-    printer(vec);
-    easyfind(vec, 8);
-
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_2 = DEQUE + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;32mFOUND\033[0m" << std::endl;
+    const int n = 10;
+    int tab[n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::deque<int> deque;
+    for (int i = 0; i < 10 ; i++)
+        deque.push_back(tab[i]);
+    printer(deque);
+    easyfind(deque, 0);
+    std::cout << std::endl;
 }
 
-void test4()
+void    test3()
 {
-    std::cout <<  "\033[1m\033[35m DEQUE TESTS" << std::endl << std::endl;
-   std::cout <<  "\033[0m\033[34m DEQUE FOUND \033[0m\033[37m" << std::endl;
-
-    std::deque<int> dq;
-    dq.push_front(1);
-    dq.push_front(2);
-    dq.push_back(3);
-    dq.push_back(4);
-    printer(dq);
-    easyfind(dq, 3);
-}
-
-void test5()
-{
-    std::cout <<  "\033[1m\033[35m DEQUE TESTS" << std::endl << std::endl;
-   std::cout <<  "\033[0m\033[34m DEQUE NOT FOUND \033[0m\033[37m" << std::endl;
-
-    std::deque<int> dq;
-    dq.push_front(1);
-    dq.push_front(2);
-    dq.push_back(3);
-    dq.push_back(4);
-    dq.pop_back();
-    dq.pop_back();
-    printer(dq);
-    easyfind(dq, 3);
-}
-
-void test6()
-{
-    std::cout <<  "\033[1m\033[35m DEQUE TESTS" << std::endl << std::endl;
-   std::cout <<  "\033[0m\033[34m EMPTY DEQUE \033[0m\033[37m" << std::endl;
-
-    std::deque<int> dq;
-  
-    printer(dq);
-    easyfind(dq, 3);
-}
-
-void test7()
-{
-    std::cout <<  "\033[1m\033[35m LIST TESTS" << std::endl << std::endl;
-   std::cout <<  "\033[0m\033[34m LIST FOUND \033[0m\033[37m" << std::endl;
-
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_3 = LIST + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;32mFOUND\033[0m" << std::endl;
+    const int n = 10;
+    int tab[n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     std::list<int> list;
-    for (int i = 0; i < 5; ++i)
-        list.push_back(i * 2);
+    for (int i = 0; i < 10 ; i++)
+        list.push_back(tab[i]);
     printer(list);
-    easyfind(list, 2);
+    easyfind(list, 5);
+    std::cout << std::endl;
 }
 
-void test8()
+void    test4()
 {
-    std::cout <<  "\033[1m\033[35m DEQUE TESTS" << std::endl << std::endl;
-   std::cout <<  "\033[0m\033[34m LIST NOT FOUND \033[0m\033[37m" << std::endl;
-
-   std::list<int> list;
-    for (int i = 0; i < 5; ++i)
-        list.push_back(i * 2);
-    printer(list);
-    easyfind(list, 3);
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_4 = VECTOR NOT FOUND + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;31mNOT FOUND\033[0m" << std::endl;
+    const int n = 10;
+    int tab[n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> vector(tab, tab + n);
+    printer(vector);
+    easyfind(vector, 11);
+    std::cout << std::endl;
 }
 
-void test9()
+void    test5()
 {
-    std::cout <<  "\033[1m\033[35m DEQUE TESTS" << std::endl << std::endl;
-   std::cout <<  "\033[0m\033[34m EMPTY LIST \033[0m\033[37m" << std::endl;
-
-   std::list<int> list;
-    printer(list);
-    easyfind(list, 2);
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_5 = DEQUE NOT FOUND + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;31mNOT FOUND\033[0m" << std::endl;
+    const int n = 10;
+    int tab[n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::deque<int> deque;
+    for (int i = 0; i < 10 ; i++)
+        deque.push_back(tab[i]);
+    printer(deque);
+    easyfind(deque, -9);
+    std::cout << std::endl;
 }
+
+void    test6()
+{
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_6 = LIST NOT FOUND + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;31mNOT FOUND\033[0m" << std::endl;
+    const int n = 10;
+    int tab[n] = {0, 1, 2, 3, 4, 10, 6, 7, 8, 9};
+    std::list<int> list;
+    for (int i = 0; i < 10 ; i++)
+        list.push_back(tab[i]);
+    printer(list);
+    easyfind(list, 5);
+    std::cout << std::endl;
+}
+
+void    test7()
+{
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_7 = EMPTY VECTOR + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;31mNOT FOUND\033[0m" << std::endl;
+    std::vector<int> list;
+    printer(list);
+    easyfind(list, 5);
+    std::cout << std::endl;
+}
+
+void    test8()
+{
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_8 = EMPTY DEQUE + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;31mNOT FOUND\033[0m" << std::endl;
+    std::deque<int> deque;
+    printer(deque);
+    easyfind(deque, 5);
+    std::cout << std::endl;
+}
+
+void    test9()
+{
+    std::cout << "\033[1;33m+ + + + + + + + + TEST_9 = EMPTY LIST + + + + + + + + +\033[0m" << std::endl;
+    std::cout << " Result expected = \033[1;31mNOT FOUND\033[0m" << std::endl;
+    std::list<int> list;;
+    printer(list);
+    easyfind(list, 5);
+    std::cout << std::endl;
+}
+
 
 int main()
 {
-   test1();
-//    test2();
-//    test3();
-//    test4();
-//    test5();
-//    test6();
-//    test7();
-//    test8();
-//    test9();
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();   
+    test8();
+    test9();
 }
