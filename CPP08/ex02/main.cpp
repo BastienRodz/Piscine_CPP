c27388eb970020f6d7ec2ad08e12c6d3815d100e
Bastien RODRIGUEZ <barodrig@e2r1p13.clusters.42paris.fr>
2022-11-03 10:55:23 +0100

    Improved CPP08/ex02 main

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,44 +6,45 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 11:38:34 by barodrig          #+#    #+#             */
-/*   Updated: 2022/11/02 12:04:16 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/11/03 10:54:33 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 #include <list>
 
-# define RESET "\e[0m"
-# define BWHT "\e[1;37m"
+# define YELLOW "\e[1;33m"
+# define RED "\e[1;31m"
 # define UCYN "\e[4;36m"
+# define STOP "\e[0m"
 
 int main()
 {
-	std::cout << BWHT<< "================= Stack Test ===============" << RESET << std::endl;
+	std::cout << YELLOW << " + + + + + Stack Test + + + + + " << STOP << std::endl;
 	
 	{
 		MutantStack<int> mstack;
 
         if (mstack.empty())
-			std::cout << "empty" << std::endl;
+			std::cout << RED << "EMPTY" << STOP << std::endl;
     
-		mstack.push(5);
-		mstack.push(17);
+		mstack.push(42);
+		mstack.push(24);
 		std::cout << "last element added : " << mstack.top() << std::endl; 
 		
 		mstack.pop();	// remove last element added
-		std::cout << "size : " << mstack.size() << std::endl;
+		std::cout << "size after removing last element added (should be 1) : " << mstack.size() << std::endl;
 		
-		mstack.push(3);
-		mstack.push(5);
-		mstack.push(737);
+		mstack.push(1);
+		mstack.push(2);
+		mstack.push(2147483647);
 		mstack.push(0);
 		
 		MutantStack<int>::iterator it = mstack.begin();
 		MutantStack<int>::iterator ite = mstack.end();
 		++it;
 		--it;
-		std::cout << UCYN << "Stack:" << RESET << std::endl;
+		std::cout << YELLOW << "Stack:" << STOP << std::endl;
 		while (it != ite)
 		{
 			std::cout << *it << std::endl;
@@ -52,7 +53,6 @@ int main()
 		
 		std::stack<int> s(mstack);
 
-		std::cout << "\n*~~~~~~~~~~~~~~ More tests ~~~~~~~~~~~~~~*" << std::endl;
 		it = mstack.begin();
 		std::cout << "first element added on the stack : " << *it << std::endl;
 		it++;
@@ -61,38 +61,37 @@ int main()
 	}
 
 
-	std::cout << BWHT<< "\n================= List Test ===============" << RESET << std::endl;
+	std::cout << YELLOW << "\n + + + + + List Test + + + + +" << STOP << std::endl;
 
 	{
 		std::list<int>	mlist;
 
 		if (mlist.empty())
-			std::cout << "empty" << std::endl;
+			std::cout << RED << "EMPTY" << STOP << std::endl;
 		
-		mlist.push_back(5);
-		mlist.push_back(17);
+		mlist.push_back(42);
+		mlist.push_back(24);
 		std::cout << "last element added : " << mlist.back() << std::endl;
 		
 		mlist.pop_back();	// remove last element added
-		std::cout << "size : " << mlist.size() << std::endl;
+		std::cout << "size after removing last element added (should be 1) : " << mlist.size() << std::endl;
 		
-		mlist.push_back(3);
-		mlist.push_back(5);
-		mlist.push_back(737);
+		mlist.push_back(1);
+		mlist.push_back(2);
+		mlist.push_back(2147483647);
 		mlist.push_back(0);
 		
 		std::list<int>::iterator it = mlist.begin();
 		std::list<int>::iterator ite = mlist.end();
 		++it;
 		--it;
-		std::cout << UCYN << "List:" << RESET << std::endl;
+		std::cout << YELLOW << "List:" << STOP << std::endl;
 		while (it != ite)
 		{
 			std::cout << *it << std::endl;
 			++it;
 		}
 
-        std::cout << "\n*~~~~~~~~~~~~~~ More tests ~~~~~~~~~~~~~~*" << std::endl;
 		it = mlist.begin();
 		std::cout << "first element added on the stack : " << *it << std::endl;
 		it++;
